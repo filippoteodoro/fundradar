5753502a5d4a71f19a2887277f09d314cf45121a
Filippo Teodoro <18514232+filippoteodoro@users.noreply.github.com>
2026-02-19 17:20:09 +0100

    Update

--- apps/web/src/components/PortfolioTable.tsx
+++ apps/web/src/components/PortfolioTable.tsx
@@ -156,11 +156,9 @@ export function PortfolioTable({ companies }: PortfolioTableProps) {
                           const displaySector = normalizePortfolioSector(company.sector) || company.sector;
                           const sc = getSectorColor(displaySector);
                           return (
-                            <div style={{ width: '180px' }}>
-                              <span style={{ ...badgeStyle({ bg: sc.bg, color: sc.text }), whiteSpace: 'normal', display: 'inline-block', lineHeight: '1.3' }}>
-                                {displaySector}
-                              </span>
-                            </div>
+                            <span style={badgeStyle({ bg: sc.bg, color: sc.text })}>
+                              {displaySector}
+                            </span>
                           );
                         })() : <span style={{ color: '#999' }}>-</span>}
                       </td>
